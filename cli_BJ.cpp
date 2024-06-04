8b14aa1cdc67e9d1a665355abe872427963e4e0c
Filcent <filcent2@gmail.com>
2024-06-04 15:33:28 +0200

    added a dealer and a player to initialize the cards first dealt, together with a bunch of other things to do.

--- cli_BJ.cpp
+++ cli_BJ.cpp
@@ -8,29 +8,65 @@ using namespace std;
 
 void printLogo();
 void startUp(int& money, int& bet);
-
+void showCards(vector<int>& dealerCards, vector<int>& playerCards);
 
 
 int main() {
 
 	int money = 200;
 	int bet = 0;
+	char choice;
+
 
 	random_device rd;
 	uniform_int_distribution<int> dist(1, 13);
+
 	vector <int> dealerCards = {};
 	vector <int> playerCards = {};
 
 	printLogo();
 	startUp(money, bet);
 
-	dealerCards.push_back(dist(rd));	//TODO: use randomly generated value from 1 (ace) to 13 (king) to assign cards 
-	for (int card : dealerCards) {
-		cout << card << ' ';
+	dealerCards.push_back(dist(rd));	//gives dealer 1 card
+
+	playerCards.push_back(dist(rd));
+	playerCards.push_back(dist(rd));	//gives player 2 cards
+
+	showCards(dealerCards, playerCards);
+
+
+	//make a function void game() that maybe can be called inside itself
+
+	cout << "[H]it, [S]tay, [D]ouble down or [G]ive up (surrender)?";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 'H': {
+		playerCards.push_back(dist(rd));
+		// check to see if all the cards combined are >21, if they are break and auto-loose, else continue (call the "game()" function)
 	}
-//	playerCards.push_back(1);	//TODO: make the card dealing repetable (duh)
+	case 'S': {
+		break;		//breaks the switch case, and makes 
+	}
+	case 'D': {
+		//if (bet*2 >=0) {bet *= 2; money -= bet;}
+		//else {cout << "Not enought money to Double down!" << endl 
+		//call the "game()" function so it restarts
+	}
+	case 'G': {
+		money += bet / 2;
+		break;
+	}
+	default:
+		cout << "invalid option! " << endl;	//says the user put in an invalid value	
+											//call the "game()" function so it restarts		
+	}
+
+
+//TODO: make the card dealing repetable (duh)
 							
-								//TODO: add a something to "translate"  1,11,12 and 13 to A,J,Q and K
+//TODO: add a something to "translate"  1,11,12 and 13 to A,J,Q and K
 	
 	system("pause");
 	return 0;
@@ -69,3 +105,19 @@ void startUp(int& money, int& bet) {
 }
 //asks the user how much they want to bet and saves and removes it from the player's money
 
+void showCards(vector<int>& dealerCards, vector<int>& playerCards) {
+	cout << "Dealer card(s): ";
+	for (int card : dealerCards) {
+		cout << card << ' ';
+	}
+
+	cout << endl;
+
+	cout << "Player cards: ";
+	for (int card : playerCards) {
+		cout << card << ' ';
+	}
+
+	cout << endl;
+}
+//shows all of the dealer and the player's cards
